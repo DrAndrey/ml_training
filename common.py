1a7b6db44b3bf01039337dab4d6c9ed5ed55ac7f
DAndrey <andreydruzhaev@gmail.com>
2016-03-25 19:21:16 +0300

    start to use weighted xgbc

--- common.py
+++ common.py
@@ -5,10 +5,15 @@
 """
 
 import os
+import multiprocessing
 
 import pylab
 import numpy as np
 import pandas as pd
+
+from sklearn.metrics import get_scorer
+
+NCPU = multiprocessing.cpu_count() - 1
 
 
 def save_output(data, labels):
@@ -39,6 +44,22 @@ def find_corr_features_mask(matr, trashhold=0.9):
                 corr_ind.append(j)
     corr_mask = np.array([False if i in corr_ind else True for i in range(n)])
     return corr_mask
+
+
+def cross_val_score_with_weights(estimator_cls, x, y, w, scoring, cv, params):
+    scores = []
+    scorer = get_scorer(scoring)
+    for train_ind, test_ind in cv:
+        estimator = estimator_cls(**params)
+        estimator.fit(x[train_ind], y[train_ind], w[train_ind])
+
+        if scoring in ["roc_auc"]:
+            y_pred = estimator.predict_proba(x[test_ind])[:, 1]
+        else:
+            y_pred = estimator.predict(x[test_ind])[:, 1]
+        scores.append(scorer(y[test_ind], y_pred))
+    return np.array(scorer)
+
 
 if __name__ == '__main__':
     pass
